a18a2a9d732044e1731fa909d836305672c95e26
oka hidenobu <okahidenobu@gmail.com>
2020-03-27 01:07:53 +0900

    firebase-nuxt-udemy

---
+++ store/index.js
@@ -0,0 +1,15 @@
+import { vuexfireMutations } from "vuexfire";
+
+export const mutations = {
+  ...vuexfireMutations
+};
+
+// import Vuex from 'vuex'
+// import { vuexfireMutations } from 'vuexfire'
+//
+// const store = new Vuex.Store({
+//   mutations: {
+//     // other mutations
+//     ...vuexfireMutations,
+//   },
+// })
