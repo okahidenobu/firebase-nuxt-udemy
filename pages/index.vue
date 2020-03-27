988cdaefb2eaae0f7636ff4e58f30546fd6aff6d
oka hidenobu <okahidenobu@gmail.com>
2020-03-27 15:53:58 +0900

    login機能実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,76 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        firebase-nuxt-udemy
-      </h1>
-      <h2 class="subtitle">
-        My doozie Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <login v-if="!isLogin" :user="userData"></login>
+    <todos v-if="isLogin" :user="userData"></todos>
   </div>
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
-
-console.log(process.env);
+import firebase from "firebase";
+import todos from "../components/todos";
+import login from "../components/login";
 
 export default {
+  name: "home",
   components: {
-    Logo
+    todos,
+    login
+  },
+  asyncData(context) {
+    // コンポーネントをロードする前に毎回呼び出されます
+    return { name: "Hello, World！！", isLogin: false, userData: null };
+  },
+  mounted: function() {
+    firebase.auth().onAuthStateChanged(user => {
+      // console.log(user);
+      if (user) {
+        this.isLogin = true;
+        this.userData = user;
+      } else {
+        this.isLogin = false;
+        this.userData = null;
+      }
+    });
+  },
+  methods: {
+    googleLogin: function() {
+      firebase
+        .auth()
+        .signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+        .then(function(result) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          // ...
+          console.log("login!!");
+          // console.log({ token, user });
+        })
+        .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+          console.log("login errrrrrror");
+        });
+    },
+    googleLogOut: () => {
+      firebase
+        .auth()
+        .signOut()
+        .then(function() {
+          // Sign-out successful.
+          console.log("logout");
+        })
+        .catch(function(error) {
+          // An error happened.
+          console.log("logout errror");
+        });
+    }
   }
 };
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
